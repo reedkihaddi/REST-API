f498cf2f257e6fd3a02acbe8896475a8adeeb327
Saurabh <saurabhbraryo@gmail.com>
2020-09-19 12:49:38 +0530

    added graceful shutdown

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/reedkihaddi/REST-API/config"
 	"github.com/reedkihaddi/REST-API/logging"
@@ -19,13 +23,47 @@ import (
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 // @host localhost:8080
 // @BasePath /
-
 func main() {
 
 	logging.InitLogger()
 	config.InitConfig()
 	router := router.NewRouter()
-	logging.Log.Info("Starting up the server.")
-	http.ListenAndServe(os.Getenv("PORT"), router)
+	
+	srv := &http.Server{
+		Addr:    os.Getenv("PORT"),
+		Handler: router,
+	}
+	
+	//Create a channel to listen for shutdown
+	signalChan := make(chan os.Signal, 1)
+	//Notify channel for signals
+	signal.Notify(
+		signalChan,
+		syscall.SIGHUP,  // sent when a program loses its controlling terminal
+		syscall.SIGINT,  // sent when controlling terminal presses the interrupt character (Control-C)
+		syscall.SIGQUIT, // sent when controlling terminal presses the quit character (Control-Backslash)
+	)
+	
+	//Listen to connections
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			logging.Log.Fatalf("listen: %s\n", err)
+		}
+	}()
+	logging.Log.Info("Server started.")
+	
+	//Block
+	<-signalChan
+	logging.Log.Info("Server stopped")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer func() {
+		cancel()
+	}()
+	
+	if err := srv.Shutdown(ctx); err != nil {
+		logging.Log.Fatalf("Server shutdown failed:%+v", err)
+	}
+	
+	logging.Log.Info("Server exited properly")
 
 }
