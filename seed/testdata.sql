18c8d0b734e366337c4d4f618f4d6e73667579e9
Saurabh <saurabhbraryo@gmail.com>
2020-09-21 00:55:21 +0530

    Update testdata.sql

--- seed/testdata.sql
+++ seed/testdata.sql
@@ -1,4 +1,6 @@
 INSERT INTO products (id, name, price)
 VALUES (1, 'Nvidia RTX 3070', 499.99),
        (2, 'Nvidia RTX 3080', 999.99),
-       (3, 'Nvidia RTX 3090', 1499.99);
+       (3, 'Nvidia RTX 3090', 1499.99),
+       (4, 'Nvidia RTX 2070', 399.99),
+       (5, 'Nvidia RTX 2080', 499.99);
